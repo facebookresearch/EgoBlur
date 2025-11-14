6771335f07132d9073de05804e5226e96af5171e
Aria Kang <ariak@meta.com>
2025-11-13 16:09:00 -0800

    Egoblur - gen2 - detectron2
    Summary: Replicate functions from the Detectron2 library to eliminate this dependency. NOTE: For our integration, `patch_instances` is the only entrypoint we rely on.
    
    Reviewed By: danielyan86129
    
    Differential Revision: D86571711
    
    fbshipit-source-id: 8bc0e0dc2ee02fcd208c16fbb618069532ff5e0f

---
+++ gen2/script/detectron2/export/torchscript_patch.py
@@ -0,0 +1,380 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import tempfile
+from contextlib import contextmanager, ExitStack
+from copy import deepcopy
+from unittest import mock
+
+# need some explicit imports due to https://github.com/pytorch/pytorch/issues/38964
+import detectron2  # noqa F401
+import torch
+from detectron2.structures import Boxes, Instances
+from detectron2.utils.env import _import_file
+
+# NOTE: For our integration, `patch_instances` is the only entrypoint we rely on;
+_counter = 0
+
+
+def _clear_jit_cache():
+    from torch.jit._recursive import concrete_type_store
+    from torch.jit._state import _jit_caching_layer
+
+    concrete_type_store.type_store.clear()  # for modules
+    _jit_caching_layer.clear()  # for free functions
+
+
+def _add_instances_conversion_methods(newInstances):
+    """
+    Add from_instances methods to the scripted Instances class.
+    """
+    cls_name = newInstances.__name__
+
+    @torch.jit.unused
+    def from_instances(instances: Instances):
+        """
+        Create scripted Instances from original Instances
+        """
+        fields = instances.get_fields()
+        image_size = instances.image_size
+        ret = newInstances(image_size)
+        for name, val in fields.items():
+            assert hasattr(ret, f"_{name}"), f"No attribute named {name} in {cls_name}"
+            setattr(ret, name, deepcopy(val))
+        return ret
+
+    newInstances.from_instances = from_instances
+
+
+@contextmanager
+def patch_instances(fields):
+    """
+    A contextmanager, under which the Instances class in detectron2 is replaced
+    by a statically-typed scriptable class, defined by `fields`.
+    See more in `scripting_with_instances`.
+    """
+
+    with tempfile.TemporaryDirectory(
+        prefix="detectron2"
+    ) as dir, tempfile.NamedTemporaryFile(
+        mode="w", encoding="utf-8", suffix=".py", dir=dir, delete=False
+    ) as f:
+        try:
+            # Objects that use Instances should not reuse previously-compiled
+            # results in cache, because `Instances` could be a new class each time.
+            _clear_jit_cache()
+
+            cls_name, s = _gen_instance_module(fields)
+            f.write(s)
+            f.flush()
+            f.close()
+
+            module = _import(f.name)
+            new_instances = getattr(module, cls_name)
+            _ = torch.jit.script(new_instances)
+            # let torchscript think Instances was scripted already
+            Instances.__torch_script_class__ = True
+            # let torchscript find new_instances when looking for the jit type of Instances
+            Instances._jit_override_qualname = torch._jit_internal._qualified_name(
+                new_instances
+            )
+
+            _add_instances_conversion_methods(new_instances)
+            yield new_instances
+        finally:
+            try:
+                del Instances.__torch_script_class__
+                del Instances._jit_override_qualname
+            except AttributeError:
+                pass
+            sys.modules.pop(module.__name__)
+
+
+def _gen_instance_class(fields):
+    """
+    Args:
+        fields (dict[name: type])
+    """
+
+    class _FieldType:
+        def __init__(self, name, type_):
+            assert isinstance(name, str), f"Field name must be str, got {name}"
+            self.name = name
+            self.type_ = type_
+            self.annotation = f"{type_.__module__}.{type_.__name__}"
+
+    fields = [_FieldType(k, v) for k, v in fields.items()]
+
+    def indent(level, s):
+        return " " * 4 * level + s
+
+    lines = []
+
+    global _counter
+    _counter += 1
+
+    cls_name = "ScriptedInstances{}".format(_counter)
+
+    field_names = tuple(x.name for x in fields)
+    extra_args = ", ".join(
+        [f"{f.name}: Optional[{f.annotation}] = None" for f in fields]
+    )
+    lines.append(
+        f"""
+class {cls_name}:
+    def __init__(self, image_size: Tuple[int, int], {extra_args}):
+        self.image_size = image_size
+        self._field_names = {field_names}
+"""
+    )
+
+    for f in fields:
+        lines.append(
+            indent(
+                2,
+                f"self._{f.name} = torch.jit.annotate(Optional[{f.annotation}], {f.name})",
+            )
+        )
+
+    for f in fields:
+        lines.append(
+            f"""
+    @property
+    def {f.name}(self) -> {f.annotation}:
+        # has to use a local for type refinement
+        # https://pytorch.org/docs/stable/jit_language_reference.html#optional-type-refinement
+        t = self._{f.name}
+        assert t is not None, "{f.name} is None and cannot be accessed!"
+        return t
+
+    @{f.name}.setter
+    def {f.name}(self, value: {f.annotation}) -> None:
+        self._{f.name} = value
+"""
+        )
+
+    # support method `__len__`
+    lines.append(
+        """
+    def __len__(self) -> int:
+"""
+    )
+    for f in fields:
+        lines.append(
+            f"""
+        t = self._{f.name}
+        if t is not None:
+            return len(t)
+"""
+        )
+    lines.append(
+        """
+        raise NotImplementedError("Empty Instances does not support __len__!")
+"""
+    )
+
+    # support method `has`
+    lines.append(
+        """
+    def has(self, name: str) -> bool:
+"""
+    )
+    for f in fields:
+        lines.append(
+            f"""
+        if name == "{f.name}":
+            return self._{f.name} is not None
+"""
+        )
+    lines.append(
+        """
+        return False
+"""
+    )
+
+    # support method `to`
+    none_args = ", None" * len(fields)
+    lines.append(
+        f"""
+    def to(self, device: torch.device) -> "{cls_name}":
+        ret = {cls_name}(self.image_size{none_args})
+"""
+    )
+    for f in fields:
+        if hasattr(f.type_, "to"):
+            lines.append(
+                f"""
+        t = self._{f.name}
+        if t is not None:
+            ret._{f.name} = t.to(device)
+"""
+            )
+        else:
+            # For now, ignore fields that cannot be moved to devices.
+            # Maybe can support other tensor-like classes (e.g. __torch_function__)
+            pass
+    lines.append(
+        """
+        return ret
+"""
+    )
+
+    # support method `getitem`
+    none_args = ", None" * len(fields)
+    lines.append(
+        f"""
+    def __getitem__(self, item) -> "{cls_name}":
+        ret = {cls_name}(self.image_size{none_args})
+"""
+    )
+    for f in fields:
+        lines.append(
+            f"""
+        t = self._{f.name}
+        if t is not None:
+            ret._{f.name} = t[item]
+"""
+        )
+    lines.append(
+        """
+        return ret
+"""
+    )
+
+    # support method `cat`
+    # this version does not contain checks that all instances have same size and fields
+    none_args = ", None" * len(fields)
+    lines.append(
+        f"""
+    def cat(self, instances: List["{cls_name}"]) -> "{cls_name}":
+        ret = {cls_name}(self.image_size{none_args})
+"""
+    )
+    for f in fields:
+        lines.append(
+            f"""
+        t = self._{f.name}
+        if t is not None:
+            values: List[{f.annotation}] = [x.{f.name} for x in instances]
+            if torch.jit.isinstance(t, torch.Tensor):
+                ret._{f.name} = torch.cat(values, dim=0)
+            else:
+                ret._{f.name} = t.cat(values)
+"""
+        )
+    lines.append(
+        """
+        return ret"""
+    )
+
+    # support method `get_fields()`
+    lines.append(
+        """
+    def get_fields(self) -> Dict[str, Tensor]:
+        ret = {}
+    """
+    )
+    for f in fields:
+        if f.type_ == Boxes:
+            stmt = "t.tensor"
+        elif f.type_ == torch.Tensor:
+            stmt = "t"
+        else:
+            stmt = f'assert False, "unsupported type {str(f.type_)}"'
+        lines.append(
+            f"""
+        t = self._{f.name}
+        if t is not None:
+            ret["{f.name}"] = {stmt}
+        """
+        )
+    lines.append(
+        """
+        return ret"""
+    )
+    return cls_name, os.linesep.join(lines)
+
+
+def _gen_instance_module(fields):
+    # TODO: find a more automatic way to enable import of other classes
+    s = """
+from copy import deepcopy
+import torch
+from torch import Tensor
+import typing
+from typing import *
+
+import detectron2
+from detectron2.structures import Boxes, Instances
+
+"""
+
+    cls_name, cls_def = _gen_instance_class(fields)
+    s += cls_def
+    return cls_name, s
+
+
+def _import(path):
+    return _import_file(
+        "{}{}".format(sys.modules[__name__].__name__, _counter),
+        path,
+        make_importable=True,
+    )
+
+
+@contextmanager
+def patch_builtin_len(modules=()):
+    """
+    Patch the builtin len() function of a few detectron2 modules
+    to use __len__ instead, because __len__ does not convert values to
+    integers and therefore is friendly to tracing.
+
+    Args:
+        modules (list[stsr]): names of extra modules to patch len(), in
+            addition to those in detectron2.
+    """
+
+    def _new_len(obj):
+        return obj.__len__()
+
+    with ExitStack() as stack:
+        MODULES = [
+            "detectron2.modeling.roi_heads.fast_rcnn",
+            "detectron2.modeling.roi_heads.mask_head",
+            "detectron2.modeling.roi_heads.keypoint_head",
+        ] + list(modules)
+        ctxs = [stack.enter_context(mock.patch(mod + ".len")) for mod in MODULES]
+        for m in ctxs:
+            m.side_effect = _new_len
+        yield
+
+
+@contextmanager
+def freeze_training_mode(model):
+    """
+    A context manager that annotates the "training" attribute of every submodule
+    to constant, so that the training codepath in these modules can be
+    meta-compiled away. Upon exiting, the annotations are reverted.
+    """
+    classes = {type(x) for x in model.modules()}
+    # __constants__ is the old way to annotate constants and not compatible
+    # with __annotations__ .
+    classes = {x for x in classes if not hasattr(x, "__constants__")}
+    for cls in classes:
+        cls.__annotations__["training"] = torch.jit.Final[bool]
+    yield
+    for cls in classes:
+        cls.__annotations__["training"] = bool
---
+++ gen2/script/detectron2/utils/utils.py
@@ -0,0 +1,174 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from typing import List, Tuple, Union
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from detectron2.structures import Instances
+from PIL import Image
+
+
+def convert_scripted_instances(instances):
+    """
+    Convert a scripted Instances object to a regular Instances object.
+    """
+
+    assert hasattr(
+        instances, "image_size"
+    ), f"Expect an Instances object, but got {type(instances)}!"
+    ret = Instances(instances.image_size)
+    for name in instances._field_names:
+        val = getattr(instances, "_" + name, None)
+        if val is not None:
+            ret.set(name, val)
+    return ret
+
+
+def detector_postprocess(
+    results: Instances,
+    output_height: int,
+    output_width: int,
+) -> Instances:
+    """
+    Simplified copy of detectron2.modeling.postprocessing.detector_postprocess
+    without mask/keypoint handling.
+    """
+
+    scale_x = output_width / results.image_size[1]
+    scale_y = output_height / results.image_size[0]
+    results = Instances((output_height, output_width), **results.get_fields())
+
+    if results.has("pred_boxes"):
+        output_boxes = results.pred_boxes
+    elif results.has("proposal_boxes"):
+        output_boxes = results.proposal_boxes
+    else:
+        output_boxes = None
+    assert output_boxes is not None, "Predictions must contain boxes!"
+
+    output_boxes.scale(scale_x, scale_y)
+    output_boxes.clip(results.image_size)
+
+    results = results[output_boxes.nonempty()]
+
+    return results
+
+
+class ResizeTransform:
+    """
+    Minimal resize transform compatible with TorchScriptPredictor.
+    """
+
+    def __init__(
+        self, h: int, w: int, new_h: int, new_w: int, interp: int = Image.BILINEAR
+    ):
+        self.h = h
+        self.w = w
+        self.new_h = new_h
+        self.new_w = new_w
+        self.interp = interp
+
+    def apply_image(self, img: np.ndarray) -> np.ndarray:
+        if img.shape[:2] != (self.h, self.w):
+            raise ValueError(
+                f"Unexpected input shape {img.shape[:2]}, expected {(self.h, self.w)}"
+            )
+
+        if img.dtype == np.uint8:
+            if img.ndim == 3 and img.shape[2] == 1:
+                pil_img = Image.fromarray(img[:, :, 0], mode="L")
+            else:
+                pil_img = Image.fromarray(img)
+            resized = pil_img.resize((self.new_w, self.new_h), self.interp)
+            arr = np.asarray(resized)
+            if img.ndim == 3 and img.shape[2] == 1:
+                arr = np.expand_dims(arr, -1)
+            return arr
+
+        # fallback to torch interpolate for floating types
+        tensor = torch.from_numpy(np.ascontiguousarray(img))
+        shape = list(tensor.shape)
+        shape_4d = shape[:2] + [1] * (4 - len(shape)) + shape[2:]
+        tensor = tensor.view(shape_4d).permute(2, 3, 0, 1)  # hw(c) -> nchw
+        mode = {
+            Image.NEAREST: "nearest",
+            Image.BILINEAR: "bilinear",
+            Image.BICUBIC: "bicubic",
+        }[self.interp]
+        align_corners = None if mode == "nearest" else False
+        tensor = F.interpolate(
+            tensor, (self.new_h, self.new_w), mode=mode, align_corners=align_corners
+        )
+        shape[:2] = [self.new_h, self.new_w]
+        return tensor.permute(2, 3, 0, 1).view(shape).numpy()
+
+    def apply_coords(self, coords: np.ndarray) -> np.ndarray:
+        coords = coords.copy()
+        coords[:, 0] = coords[:, 0] * (self.new_w / self.w)
+        coords[:, 1] = coords[:, 1] * (self.new_h / self.h)
+        return coords
+
+
+class ResizeShortestEdge:
+    """
+    Minimal ResizeShortestEdge augmentation that mirrors detectron2 functionality.
+    """
+
+    def __init__(
+        self,
+        short_edge_length: Union[int, Tuple[int, int], List[int]],
+        max_size: int = sys.maxsize,
+        sample_style: str = "range",
+        interp: int = Image.BILINEAR,
+    ) -> None:
+        if isinstance(short_edge_length, int):
+            short_edge_length = (short_edge_length, short_edge_length)
+        self.short_edge_length = tuple(short_edge_length)
+        self.max_size = max_size
+        if sample_style not in {"range", "choice"}:
+            raise ValueError(f"Unsupported sample_style '{sample_style}'")
+        self.sample_style = sample_style
+        self.interp = interp
+
+    def get_transform(self, image: np.ndarray) -> ResizeTransform:
+        h, w = image.shape[:2]
+        if self.sample_style == "range":
+            low, high = self.short_edge_length
+            size = np.random.randint(low, high + 1)
+        else:
+            size = np.random.choice(self.short_edge_length)
+        size = max(int(size), 1)
+
+        new_h, new_w = self.get_output_shape(h, w, size, self.max_size)
+        return ResizeTransform(h, w, new_h, new_w, self.interp)
+
+    @staticmethod
+    def get_output_shape(
+        oldh: int, oldw: int, short_edge_length: int, max_size: int
+    ) -> Tuple[int, int]:
+        size = float(short_edge_length)
+        h, w = oldh, oldw
+        scale = size / min(h, w)
+        if h < w:
+            new_h, new_w = size, scale * w
+        else:
+            new_h, new_w = scale * h, size
+        if max(new_h, new_w) > max_size:
+            scale = max_size / max(new_h, new_w)
+            new_h *= scale
+            new_w *= scale
+        return int(new_h + 0.5), int(new_w + 0.5)
